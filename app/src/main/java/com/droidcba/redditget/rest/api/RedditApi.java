731751201f20649e259f07f3b4bc03408e99d40c
juanchosaravia@gmail.com <SchCordoba1!>
2014-11-22 10:54:51 -0300

    fixed search top items

--- app/src/main/java/com/droidcba/redditget/rest/api/RedditApi.java
+++ app/src/main/java/com/droidcba/redditget/rest/api/RedditApi.java
@@ -12,6 +12,6 @@ import rx.Observable;
 public interface RedditApi {
 
     //http://www.reddit.com/r/google/top.json?limit=5
-    @GET("/r/google/top.json?limit=10")
+    @GET("/top.json?limit=10")
     Observable<Listing> getTop(@Query("after") String after);
 }
